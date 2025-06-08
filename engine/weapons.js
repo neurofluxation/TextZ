c4aa6d9a2d949e65a8674507a4ce03d8a64aade4
Neurofluxation <betanuhack@gmail.com>
2025-06-08 15:17:43 +0100

    Made Rifle actually work...

--- engine/weapons.js
+++ engine/weapons.js
@@ -4,6 +4,7 @@ var WEAPONS = {
     knife: { name: 'Knife', damage: 20 },
     leadpipe: { name: 'Lead Pipe', damage: 25 },
     pistol: { name: 'P1 Pistol', damage: 30 },
+    rifle: { name: '.22 Rifle', damage: 32 },
     glock: { name: 'Glock Pistol', damage: 35 },
     mosinRifle: { name: 'Mosin Rifle', damage: 40 },
     m4a1: { name: 'M4A1', damage: 50 }
